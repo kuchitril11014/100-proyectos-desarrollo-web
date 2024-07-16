c31b4b3c820fbf5d8defee0bd314c48d676a27e7
kuchitril11014 <rafaelsoto11014@outlook.com>
2024-07-15 17:38:59 -0700

    repaso js

--- Repaso Javascript/main.js
+++ Repaso Javascript/main.js
@@ -6,20 +6,18 @@ let nombre = 'rafael'
 nombre = 'victor'
 
 //constantes
-const altura = 187;
 const apellido = 'Robles';
-// altura = 182;
+
+//otra variable
+ let altura = prompt('cuanto mides?');
 
 //mostrar por consola
 console.log(nombre);
 console.log(altura);
+
 //concatenacion
-
 let concatenacion = nombre + " " + apellido;
-
 console.log(concatenacion)
-
-
 //seleccionar elementos de la pagina
 
 let datos = document.querySelector('#datos');
@@ -30,5 +28,25 @@ datos.innerHTML = `
     <h3>Mido : ${altura} cm</h3>
 `;
 
-//condiciones 
+// Condicionales
 
+if (altura >= 175) {
+    datos.innerHTML += "<h1>Eres una persona alta </h1>"
+}else{
+    datos.innerHTML += "<h1>eres una persona bajita</h1>"
+};
+
+// bucles
+// for(inicializacdor; condicion; modificador){
+
+// }
+
+for(let year = "2000"; year <= 2023; year++){
+    datos.innerHTML += `<h2>Estamos en el año ${year}</h2>`
+}
+
+//Arrays
+let nombres = ["Rafael","Francisco","pepe"];
+
+let divNombres = document.querySelector("#nombres");
+divNombres.innerHTML = nombres[2];
